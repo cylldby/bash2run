c72b413cd5ea3473c4f7cc481899de6968667476
cylldby <cylldy.dev@gmail.com>
2021-04-30 18:22:43 +0900

    ♻️ (Main): Refactoring payload reading based on its length
    The previous method didn't consider nested json payloads. Since the length of the payload is accessible through the HTTP_CONTENT_LENGTH, it is possible to acquire the payload precisely by *read*ing precisely the number of characters.

--- main.sh
+++ main.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
-read -d } PAYLOAD;
-echo $PAYLOAD}
+read -n$HTTP_CONTENT_LENGTH PAYLOAD;
+echo $PAYLOAD
 # The contents of the payload are extracted by stripping the headers of the request
 # Then every entry of the json is exported :
 # export KEY=VALUE
 
-for s in $(echo $PAYLOAD} | \
-        sed '/./{H;$!d} ; x ; s/^[^{]*//g' | \
-        sed '/./{H;$!d} ; x ; s/[^}]*$//g' | \
+for s in $(echo $PAYLOAD | 
         jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" ); 
 do     
     export $s; 
